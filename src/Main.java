fb45175041aaabef1bfb9b1a97976990b4876bb5
Denis Siliukov <abhem21rus@mail.ru>
2024-06-16 18:28:37 +0300

    Added A1-A2 (feature)

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("A1");
+        System.out.println("A1-A2");
     }
 }
