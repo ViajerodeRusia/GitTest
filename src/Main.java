6cae5818bacbbbb1f7e054104351d63cd731271e
Denis Siliukov <abhem21rus@mail.ru>
2024-06-16 20:45:14 +0300

    Added A1-A2-A3 (feature)

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("A1");
         System.out.println("A1-A2");
+        System.out.println("A1-A2-A3");
     }
 }
